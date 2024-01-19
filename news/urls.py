c670bd1c0ff11ce19757c3524cbbf25a9eac8d01
Naiara Martins <naiaraxf@gmail.com>
2024-01-19 18:59:03 -0300

    Task 5 done

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from news.views import index
+from news.views import index, news_details
 
 
-urlpatterns = [path("", index, name="home-page")]
+urlpatterns = [
+    path("", index, name="home-page"),
+    path("news/<int:id>", news_details, name="news-details-page"),
+]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from news.models import News
 
 
 def index(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def news_details(request, id):
+    try:
+        news = get_object_or_404(News, id=id)
+        categories = news.categories.all()
+        author = news.author
+        return render(
+            request,
+            "news_details.html",
+            {"news": news, "categories": categories, "author": author},
+        )
+    except Http404:
+        return render(request, "404.html")
